34150618bf786a48dd9aaf251a1b8cea5719cee5
skyfy16 <colin.viatte@gmail.com>
2023-04-13 10:22:17 +0200

    working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,11 @@
 @file:Suppress("GradlePackageVersionRange")
 
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 val transitiveInclude: Configuration by configurations.creating
-
-fun DependencyHandlerScope.includeTransitive(
-    root: ResolvedDependency?,
-    dependencies: Set<ResolvedDependency>,
-    fabricLanguageKotlinDependency: ResolvedDependency,
-    checkedDependencies: MutableSet<ResolvedDependency> = HashSet()
-) {
-    dependencies.forEach {
-        if (checkedDependencies.contains(it) || (it.moduleGroup == "org.jetbrains.kotlin" && it.moduleName.startsWith("kotlin-stdlib")) || (it.moduleGroup == "org.slf4j" && it.moduleName == "slf4j-api"))
-            return@forEach
-
-        if (fabricLanguageKotlinDependency.children.any { kotlinDep -> kotlinDep.name == it.name }) {
-            println("Skipping -> ${it.name} (already in fabric-language-kotlin)")
-        } else {
-            include(it.name)
-            println("Including -> ${it.name} from ${root?.name}")
-        }
-        checkedDependencies += it
-
-        includeTransitive(root ?: it, it.children, fabricLanguageKotlinDependency, checkedDependencies)
-    }
-}
-
-// from : https://github.com/StckOverflw/TwitchControlsMinecraft/blob/4bf406893544c3edf52371fa6e7a6cc7ae80dc05/build.gradle.kts
-fun DependencyHandlerScope.handleIncludes(project: Project, configuration: Configuration) {
-    includeTransitive(
-        null,
-        configuration.resolvedConfiguration.firstLevelModuleDependencies,
-        project.configurations.getByName("modImplementation").resolvedConfiguration.firstLevelModuleDependencies
-            .first { it.moduleGroup == "net.fabricmc" && it.moduleName == "fabric-language-kotlin" }
-    )
-}
 
 plugins {
     id("fabric-loom") version "1.1-SNAPSHOT"
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "1.8.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
 }
 
 base {
@@ -88,24 +54,108 @@ tasks {
         }
     }
 
-    java { withSourcesJar() }
+    loom {
+        runs {
+            this.getByName("client") {
+                runDir = "testclient"
 
-    named<KotlinCompile>("compileKotlin") {
-        kotlinOptions.jvmTarget = javaVersion.toString()
+                val file = File("preconfiguration/doneclient.txt")
+                if (!file.exists()) {
+                    println("copying to client")
+                    file.createNewFile()
+
+                    // Copy some default files to the test client
+                    copy {
+                        from("preconfiguration/prepared_client/.")
+                        into("testclient")
+                        include("options.txt") // options.txt with my favorite settings
+                    }
+
+                    // Copying the world to use
+                    copy {
+                        from("preconfiguration/worlds/.")
+                        include("testworld#1/**")
+                        into("testclient/saves")
+                    }
+
+                    // Copying useful mods
+                    copy {
+                        from("preconfiguration/mods/client/.", "preconfiguration/mods/both/.")
+                        include("*.jar")
+                        into("testclient/mods")
+                    }
+
+                }
+            }
+            this.getByName("server") {
+                runDir = "testserver"
+
+                val file = File("preconfiguration/doneserver.txt")
+                if (!file.exists()) {
+                    file.createNewFile()
+                    println("copying to server")
+
+                    // Copy some default files to the test server
+                    copy {
+                        from("preconfiguration/prepared_server/.")
+                        include("server.properties") // server.properties configured with usefully settings
+                        include("eula.txt") // Accepted eula
+                        into("testserver")
+                    }
+
+                    // Copying the world to use
+                    copy {
+                        from("preconfiguration/worlds/.")
+                        include("testworld#1/**")
+                        into("testserver")
+                    }
+
+                    // Copying useful mods
+                    copy {
+                        from("preconfiguration/mods/server/.", "preconfiguration/mods/both/.")
+                        include("*.jar")
+                        into("testserver/mods")
+                    }
+                }
+            }
+        }
     }
 
-    named<JavaCompile>("compileJava") {
+    java {
+        toolchain {
+//            languageVersion.set(JavaLanguageVersion.of(javaVersion.toString()))
+//            vendor.set(JvmVendorSpec.BELLSOFT)
+        }
+        withSourcesJar()
+        withJavadocJar()
+    }
+
+    named<Wrapper>("wrapper") {
+        gradleVersion = "8.1"
+        distributionType = Wrapper.DistributionType.BIN
+    }
+
+    named<Javadoc>("javadoc") {
+        options {
+            (this as CoreJavadocOptions).addStringOption("Xdoclint:none", "-quiet")
+        }
+    }
+
+    named<Jar>("jar") {
+        from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } }
+    }
+
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+//        kotlinOptions.freeCompilerArgs += "-Xskip-prerelease-check" // Required by others project like SilkMC. Also add this to intellij setting under Compiler -> Kotlin Compiler -> Additional ...
+    }
+
+    withType<JavaCompile>().configureEach {
         options.encoding = "UTF-8"
         options.release.set(javaVersion.toString().toInt())
     }
 
-    named<Jar>("jar") {
-        from("LICENSE") {
-            rename { "${it}_${base.archivesName.get()}" }
-        }
-    }
-
-    named<Test>("test") {
+    named<Test>("test") {// https://stackoverflow.com/questions/40954017/gradle-how-to-get-output-from-test-stderr-stdout-into-console
         useJUnitPlatform()
 
         testLogging {
@@ -115,8 +165,9 @@ tasks {
     }
 
     val copyJarToTestServer = register("copyJarToTestServer") {
-        println("copy to server")
-        copyFile("build/libs/MariaDBServerFabricMC-${project.properties["mod_version"]}.jar", project.property("testServerModsFolder") as String)
+        println("copying jar to server")
+//        copyFile("build/libs/${project.properties["archives_name"]}-${project.properties["mod_version"]}.jar", project.property("testServerModsFolder") as String)
+//        copyFile("build/libs/${project.properties["archives_name"]}-${project.properties["mod_version"]}.jar", project.property("testClientModsFolder") as String)
     }
 
     build { doLast { copyJarToTestServer.get() } }
@@ -124,3 +175,35 @@ tasks {
 }
 
 fun copyFile(src: String, dest: String) = copy { from(src);into(dest) }
+
+fun DependencyHandlerScope.includeTransitive(
+    root: ResolvedDependency?,
+    dependencies: Set<ResolvedDependency>,
+    fabricLanguageKotlinDependency: ResolvedDependency,
+    checkedDependencies: MutableSet<ResolvedDependency> = HashSet()
+) {
+    dependencies.forEach {
+        if (checkedDependencies.contains(it) || (it.moduleGroup == "org.jetbrains.kotlin" && it.moduleName.startsWith("kotlin-stdlib")) || (it.moduleGroup == "org.slf4j" && it.moduleName == "slf4j-api"))
+            return@forEach
+
+        if (fabricLanguageKotlinDependency.children.any { kotlinDep -> kotlinDep.name == it.name }) {
+            println("Skipping -> ${it.name} (already in fabric-language-kotlin)")
+        } else {
+            include(it.name)
+            println("Including -> ${it.name} from ${root?.name}")
+        }
+        checkedDependencies += it
+
+        includeTransitive(root ?: it, it.children, fabricLanguageKotlinDependency, checkedDependencies)
+    }
+}
+
+// from : https://github.com/StckOverflw/TwitchControlsMinecraft/blob/4bf406893544c3edf52371fa6e7a6cc7ae80dc05/build.gradle.kts
+fun DependencyHandlerScope.handleIncludes(project: Project, configuration: Configuration) {
+    includeTransitive(
+        null,
+        configuration.resolvedConfiguration.firstLevelModuleDependencies,
+        project.configurations.getByName("modImplementation").resolvedConfiguration.firstLevelModuleDependencies
+            .first { it.moduleGroup == "net.fabricmc" && it.moduleName == "fabric-language-kotlin" }
+    )
+}
